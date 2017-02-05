93d3dac911beefdc39e5fab36d490da3f7e694da
Sergey Kazakov <tocher@yandex-team.ru>
2017-02-05 18:17:59 +0300

    preview

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).on('ready', function() {
 });
 
 // портфолио
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+$(document).on('click', '.portfolio a', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 
@@ -32,4 +32,38 @@ $('.share').click(function(e) {
     try {
         shareTmp.find('a')[0].click();
     } catch (e) {}
+});
+
+var portfolioItem = '<div class="col-md-3 col-sm-4 col-6 portfolio-item portfolio-hidden">' +
+        '<a href="portfolio/1.jpg">' +
+            '<img src="portfolio/1.jpg" class="img-fluid">' +
+            '<div class="portfolio-caption text-center lato-light">' +
+                '<div class="portfolio-title">PLAN X</div>' +
+                '<div class="portfolio-subtitle">клуб Galaxy (Гродно)</div>' +
+                '<div class="portfolio-date">19 ноября</div>' +
+            '</div>' +
+        '</a>' +
+    '</div>';
+
+$('.portfolio-more').click(function() {
+    $('.portfolio-row').append(portfolioItem);
+    $('.portfolio-row').append(portfolioItem);
+    $('.portfolio-row').append(portfolioItem);
+    $('.portfolio-row').append(portfolioItem);
+
+    setTimeout(function() {
+        $('.portfolio-hidden').removeClass('portfolio-hidden');
+    }, 100);
+});
+
+var section1Height = $('.section1').innerHeight(),
+    info = $('.info'),
+    infoFixed = $('.info_fixed');
+
+$(document).on('scroll', function(e) {
+    if (section1Height < window.scrollY) {
+        infoFixed.show();
+    } else {
+        infoFixed.hide();
+    }
 });
